2cae3c58f1658c95b9f44370c652cb5a2e395af9
Zachary <zvcoding@gmail.com>
2021-12-05 14:37:42 -0500

    it works

--- index.js
+++ index.js
@@ -17,17 +17,22 @@ const PLAYER_SIZE = 10;
 const FOV = toRadians(60);
 
 const COLORS = {
-  rays:"FF00E8"
+  floor: "#6EF0FF",
+  ceiling: "#F5FFB3",
+  wall:"#FFFF00",
+  wallDark:"#E8FF59",
+  rays:"#FF002B",
+
 }
 
 const map = [
-  [1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 1, 0, 1],
-  [1, 0, 1, 0, 1, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1],
 ];
 
 const player = {
@@ -38,7 +43,7 @@ const player = {
 }
 
 function clearScreen(){
-  context.fillStyle = "#54fbff"
+  // context.fillStyle = ""
   context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 }
 
@@ -92,11 +97,48 @@ function getVCollision(angle){
   return { angle, distance: distance(player.x, player.y, nextX, nextY), vertical:true}
 }
 
+function getHCollision(angle){
+  const up = Math.abs(Math.floor(angle / Math.PI) % 2);
+
+  const firstY = up
+  ? Math.floor(player.y / CELL_SIZE) * CELL_SIZE
+  : Math.floor(player.y / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
+
+  const firstX = player.x + (firstY - player.y) /Math.tan(angle);
+
+  const yA = up ? -CELL_SIZE : CELL_SIZE;
+  const xA = yA/ Math.tan(angle)
+
+  let wall; 
+  let nextX = firstX;
+  let nextY = firstY;
+
+  while(!wall){
+    const cellX = Math.floor(nextX / CELL_SIZE);
+    const cellY = up
+    ? Math.floor(nextY / CELL_SIZE)- 1
+    : Math.floor(nextY / CELL_SIZE);
+ 
+    if(outOfMapBounds(cellX,cellY)){
+      break
+    }
+
+    wall = map[cellY][cellX]
+    if(!wall){
+      nextX += xA
+      nextY += yA
+    }
+
+  }
+
+  return { angle, distance: distance(player.x, player.y, nextX, nextY), vertical:false}
+
+
+}
+
 function castRay(angle){
   const vCollision = getVCollision(angle);
-  // const hCollision = getHCollision(angle);
-
-  return vCollision
+  const hCollision = getHCollision(angle);
 
   return hCollision.distance >= vCollision.distance ? vCollision : hCollision
 }
@@ -112,14 +154,47 @@ function getRays(){
   });
 }
 
-function renderScene(rays){}
+function fixFishEye(distance, angle, playerAngle){
+
+  const diff = angle - playerAngle;
+  return distance * Math.cos(diff)
+
+}
+
+
+
+function renderScene(rays){
+  rays.forEach((ray, i) => {
+    const distance = fixFishEye(ray.distance,ray.angle,player.angle);
+    const wallHeight = ((CELL_SIZE * 5) / distance) * 277;
+    context.fillStyle = ray.vertical ? COLORS.wallDark : COLORS.wall;
+    context.fillRect(i, SCREEN_HEIGHT / 2 - wallHeight, 1, wallHeight)
+
+    context.fillStyle = COLORS.floor;
+    context.fillRect(
+      i,
+      SCREEN_HEIGHT / 2 + wallHeight /2,
+      1,
+      SCREEN_HEIGHT /2 - wallHeight /2
+      );
+    
+    context.fillStyle = COLORS.ceiling;
+    context.fillRect(
+      i,
+      0,
+      1,
+      SCREEN_HEIGHT /2 - wallHeight /2
+      );
+  } )
+
+}
 
 function renderMinimap(posX = 0,posY = 0, scale = 1, rays){
   const cellSize = scale * CELL_SIZE;
   map.forEach((row,y) => {
     row.forEach( (cell,x) => {
       if(cell){
-        context.fillStyle = "#fff821"
+        context.fillStyle = "#62FF55"
         context.fillRect(posX + x * cellSize, posY + y * cellSize, cellSize, cellSize)
       }
     })
@@ -138,7 +213,7 @@ function renderMinimap(posX = 0,posY = 0, scale = 1, rays){
     context.stroke()
     })
 
-  context.fillStyle="#ff2121"
+  // context.fillStyle="#ffffff"
   context.fillRect(
     posX + player.x * scale - PLAYER_SIZE/2,
     posY + player.y * scale - PLAYER_SIZE/2,
